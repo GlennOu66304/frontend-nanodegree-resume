4069a95a357bda1aa7e1d8b27a4279e3011e0b6a
Kent <Kent@example.com>
2022-03-04 17:21:12 +0800

    'bug'

--- js/helper.js
+++ js/helper.js
@@ -13,15 +13,15 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile= '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">Email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">Twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">Github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">Location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -49,7 +49,8 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineCoursesStart = '<div class="onlineCourse-entry"></div>';
+var HTMLonlineClasses = '<h3 class="onlineClass">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,66 +1,41 @@
 
 var bio = {
-  "name": "Glen Ou",
-"role": "Web Developer",
+"name": "Glenn Ou",
+"role": "Full Stack Developer",
+"welcomeMessage": "I'm currently working as a Full Stack Developer in the Beijing China. I love solving problems and providing solutions that save people time and money and help their day to day lives. Thanks for stopping by!",
+"biopic": "images/fry.jpg",
+
 "contacts":
 {
-"mobile": "18513763652",
-"email": "2738328919@qq.com",
-"github": "https://github.com/Glenou",
-"location": "Washington, D.C."
+// "mobile": "+1 7607336789",
+"email": "glenou@yahoo.com",
+"github": "@GlennOu66304",
+"twitter":"@Glenn53152781",
+"location": "Beijing China"
 },
 "skills": [
-"HTML", "CSS", "JavaScript", "jQuery", "Git", "Gulp", "Bootstrap"
+"HTML CSS Javascript jQuery", "Git Gulp Bootstrap"
 ],
-"welcomeMessage": "This is it!",
-"biopic": "images/fry.jpg"
+
 };
 
-var education = {
-"schools": [
-{
-"name": "XDF",
-"location": "Beijing,China",
-"degree": "English Spoken Leval 4",
-"majors": ["English Spoken"],
-"dates": '2017',
-"url": "https://www.gwu.edu"
-},
-{
-"name": "Beijing Waishi schools",
-"location": "Beijing, China",
-"degree": "BA",
-"majors": ["Coffee Advance"],
-"dates": '2016',
-"url": "http://www.yit.edu.cn/"
-}
-],
-"onlineCourses": [
-{
-"title": "Front End Web Developer",
-"school": "Udacity",
-"dates": '2017',
-"url": "http://cn.udacity.com/course/front-end-web-developer-nanodegree--nd001-cn-basic"
-}
-]
-};
 
 var work = {
 "jobs": [
 {
-"employer": "Yiyun Technology Service and Consulting / IT Consulting / China (Remote)",
+"employer": "Yiyun Technology Service and Consulting",
 "title": "Web Development Intern",
-"dates": "April 2016 - July 2016",
+"dates": "April 2018 - July 2019",
 "description": "Develop web pages using HTML5 technologies",
-"location": "shandong"
+"location": "Jinan Shandong"
 
 },
 {
 "employer": "DC Bilingual Public Charter School",
 "title": "Volunteer",
-"dates": "April 2017",
+"dates": "April 2017 - April 2018 ",
 "description": "Leading Chinese Culture Club among students who are interested in Chinese culture",
-"location": "hebei"
+"location": "Shijiazhuang Hebei"
 
 }]
 };
@@ -68,48 +43,120 @@ var work = {
 var projects = {
 "projects": [
 {
-"title": "Alipay",
-"dates": "December 2016",
+"title": "Alipay API",
+"dates": "December 2016 - December 2017 ",
 "description": "Interface design of application that takes a college graduate student's income and payout data to help the user identify their spending patterns, monitor the rate at which they are spending their income and encourage them to save.",
 "images": [
-"images/fry.jpg", "images/197x148.gif"
+"images/fry.jpg"
 ]
 },
 {
-"title": "Electronic resume",
-"dates": "July 2017",
+"title": "Digital Resume",
+"dates": "July 2017 - August 2017",
 "description": "Using HTML, CSS, JavaScript, Json and jQuery to build an electronic resume.",
 "images": [
-"images/fry.jpg", "images/197x148.gif"
+"images/fry.jpg"
 ]
 }
 ]
 };
-bio.display = function(){
-if(bio){
+
+var education = {
+	"schools": [
+	{
+	"name": "Harvard University",
+	"location": "Beijing,China",
+	"degree": "English Spoken Level 4",
+	"majors": ["English Spoken"],
+	"dates": "July 2017 - August 2017",
+	"url": "https://www.gwu.edu"
+	},
+
+	{
+	"name": "Yale schools",
+	"location": "Beijing, China",
+	"degree": "BA",
+	"majors": ["Coffee Making"],
+	"dates": "July 2017 - August 2017",
+	"url": "http://www.yit.edu.cn/"
+	}
+
+	],
+
+
+	"onlineCourses": [
+	{
+	"title": "Front End Web Developer",
+	"school": "Udacity",
+	"dates": "July 2017 - August 2017",
+	"url": "http://cn.udacity.com/course/front-end-web-developer-nanodegree--nd001-cn-basic"
+	},
+	{
+		"title": "AI Developer",
+		"school": "Udacity",
+		"dates": "July 2017 - August 2017",
+		"url": "http://cn.udacity.com/course/front-end-web-developer-nanodegree--nd001-cn-basic"
+		}
+	]
+
+	};
+
+	var Courses = {
+		
+	"onlineCourses": [
+		{
+		"title": "Front End Web Developer",
+		"school": "Udacity",
+		"dates": "July 2017 - August 2017",
+		"url": "http://cn.udacity.com/course/front-end-web-developer-nanodegree--nd001-cn-basic"
+		},
+		{
+			"title": "AI Developer",
+			"school": "Udacity",
+			"dates": "July 2017 - August 2017",
+			"url": "http://cn.udacity.com/course/front-end-web-developer-nanodegree--nd001-cn-basic"
+			}
+		]
+	
+	}
+
+//bio part
+
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio. welcomeMessage);
-$("#header").append(formattedName,formattedRole,formattedBioPic,formattedMessage);
-};
-if(bio.contacts){
-  var fortmattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile)
-  var fortmattedEmail = HTMLemail.replace("%data%",bio.contacts.email)
-  var fortmattedGithub = HTMLgithub.replace("%data%",bio.contacts.github)
-  var fortmattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter)
-  $("#topContacts").append(fortmattedMobile,fortmattedEmail,fortmattedGithub,fortmattedTwitter);
-  $("#footerContacts").append(fortmattedMobile,fortmattedEmail,fortmattedGithub,fortmattedTwitter);
+var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 
-}
-if(bio.skills){
-$("#header").append(HTMLskillsStart);
+//name,role,welcomemessage,biopic insert
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedMessage);
 
-var formattedSkill = HTMLskills.replace("%data%", bio.skills);
-$("#skills").append(formattedSkill);
+//contact infomation replace
+
+var formattedContactInfo=[]
+formattedContactInfo.push(HTMLemail.replace("%data%",bio.contacts.email))
+formattedContactInfo.push(HTMLgithub.replace("%data%",bio.contacts.github))
+formattedContactInfo.push(HTMLtwitter.replace("%data%",bio.contacts.twitter))
+formattedContactInfo.push(HTMLlocation.replace("%data%",bio.contacts.location)) 
+
+// top and foot contacts 
+  for(i in formattedContactInfo) {
+ $("#topContacts").append(formattedContactInfo[i]);
+ $("#footerContacts").append(formattedContactInfo[i]);
 }
-}
-bio.display();
+
+// skill part
+if(bio.skills.length >0 ){
+
+	$("#header").append(HTMLskillsStart);
+	
+	for (i in bio.skills){
+	$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+	}
+	}
+
 
 
 work.display = function(){
@@ -134,18 +181,26 @@ $("#projects").append(HTMLprojectStart);
 var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
 var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
 var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+$(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription,);
+
 projects.projects[i].images.forEach(function(imgUrl){
     $(".project-entry:last").append(HTMLprojectImage.replace("%data%",imgUrl))
 })
-$(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription,);
 }
 }
+
 projects.display();
+
+//education part
 education.display = function(){
+	//university part
+	$("#education").append(HTMLschoolStart);
+	// console.log(HTMLschoolStart)
 for (var i = 0; i < education.schools.length; i++){
-				$("#education").append(HTMLschoolStart);
+				
 				//学校和学位名称
-        var formattedName = HTMLschoolName.replace("%data%",education.schools[i].name);
+                var formattedName = HTMLschoolName.replace("%data%",education.schools[i].name);
 				var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[i].degree);
 				var formattedNameDegree = formattedName + formattedDegree;
 				//日期
@@ -155,13 +210,22 @@ for (var i = 0; i < education.schools.length; i++){
 				//地点
 				var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
 
-				//插入网页
+				//插入学校和学位名称,日期,专业,地点
 				$(".education-entry:last").append(formattedNameDegree,formattedDates,formattedMajors,formattedLocation);
 			}
 
-      for (var i = 0; i < education.onlineCourses.length; i++){
-$(".education-entry:last").append(HTMLonlineClasses)
 
+     }
+    education.display();
+
+
+//Online Course Part
+//Course this name is from the var Courses
+Courses.display = function(){
+	$("#onlineClass").append(HTMLonlineCoursesStart)
+  console.log(HTMLonlineCoursesStart)
+	// online course part
+      for (var i = 0; i < Courses.onlineCourses.length; i++){
 				//专业和学校名称
 				var formattedTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[i].title);
 				var formattedSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[i].school);
@@ -171,11 +235,15 @@ $(".education-entry:last").append(HTMLonlineClasses)
 				//网址
 				var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
 
-				//插入网页
-				$(".education-entry:last").append(formattedTitleSchool,formattedDates,formattedUrl);
+				//插入学校和学位名称,日期,专业,地点
+				$(".onlineCourse-entry:last").append(formattedTitleSchool,formattedDates,formattedUrl);
 			}
-    }
-    education.display();
+
+
+}
+
+Courses.display()
+
       // 检测点击坐标
       	$(document).click(function(loc) {
       		var x = loc.pageX;
